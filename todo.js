ad22f1be5301191ee5112e366de259751456359f
EversonA <47793920+EversonA@users.noreply.github.com>
2020-05-18 23:22:33 -0300

    Add files via upload

--- todo.js
+++ todo.js
@@ -30,16 +30,56 @@ function renderTodos() {
 
 renderTodos();
 
+if (todos.length >= 30 && todos.length <= 40) {
+    alert("seloco kkkkkk muita coisa vei");
+}
+
+if (todos.length >= 40 && todos.length <= 49) {
+    alert("Com 50 itens a lista é deletada...");
+}
+
+if (todos.length >= 50) {
+    localStorage.clear();
+}
+
+console.log(todos.length);
+
+function contator() {
+
+    if (todos.length === 5) {
+        alert("Nunca testei ate onde isso vai");
+    }
+    if (todos.length === 10) {
+        alert("Ta bem feio, eu sei");
+    }
+    if (todos.length === 15) {
+        alert("We are looking for graphic designer");
+    }
+    if (todos.length === 20) {
+        var video = document.createElement('video');
+
+        video.src = 'audio/bota.mp4';
+        video.autoplay = true;
+        video.width= "1360"; 
+        video.height= "986";
+        var videovazio = document.getElementById("video");
+        document.body.insertBefore(video, videovazio);
+    }
+    console.log(todos.length);
+}
+
 function addTodo() {
     var todoText = inputElement.value;
 
     todos.push(todoText);
     inputElement.value = '';
     renderTodos();
-    saveToStorage()
+    saveToStorage();
+    contator();
 }
 
 buttonElement.onclick = addTodo;
+
 
 
 function deleteTodo(pos) {
